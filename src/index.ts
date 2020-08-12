4a215b4af8114b144fd88a08ccd520819c1eb84c
Universal Studio <uni@un1.pw>
2020-08-12 21:05:07 +0200

    Fix security imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+import * as path from 'path'
+
 import rules from './providers'
+
+function getRulePackageDirectory (name: string, suffix: string = '') {
+  const packagePath = require.resolve(name)
+  const nodeModulesPath = path.dirname(packagePath)
+
+  return path.join(nodeModulesPath, suffix)
+}
 
 module.exports = {
   extends: ['tslint-config-standard'],
-  rules
+  rules,
+  rulesDirectory: [
+    getRulePackageDirectory('tslint-config-security', 'dist/rules'),
+    getRulePackageDirectory('tslint-eslint-rules', 'dist/rules')
+  ]
 }
