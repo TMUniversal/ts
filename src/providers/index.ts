595ea4a4634edde34f0d8d4af95ec804aac0f54a
Universal Studio <uni@un1.pw>
2020-09-08 19:10:27 +0200

    feat: remove tslint config

--- src/providers/index.ts
+++
@@ -1,7 +0,0 @@
-import tslint from './tslint'
-import tslintConfigSecurity from './tslint-config-security'
-
-export default {
-  ...tslint,
-  ...tslintConfigSecurity
-}
--- src/providers/tslint-config-security.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  'tsr-detect-buffer-noassert': true,
-  'tsr-detect-non-literal-buffer': true,
-  'tsr-detect-non-literal-regexp': true,
-  'tsr-detect-non-literal-require': true,
-  'tsr-detect-pseudo-random-bytes': true,
-  'tsr-detect-sql-literal-injection': true,
-  'tsr-detect-unsafe-regexp': true
-}
